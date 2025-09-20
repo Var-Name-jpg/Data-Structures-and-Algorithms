08e687b1c927cc58fed0b5a8e6af5998e6f869e9
Var-Name-jpg <ethanwalton0418@outlook.com>
2025-09-20 12:42:56 -0400

    added list generation and fixed sorting helpers

--- Algorithm-Analysis/Program.cs
+++ Algorithm-Analysis/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingBenchmark {
+	public class Program {
+		public static void Main(String[] args) {
+			Random rand = new Random();
+
+			// 1k lists
+			List<int> Sorted_1000 = new List<int>();
+			List<int> Random_1000 = new List<int>();
+			List<int> Reverse_1000 = new List<int>();
+
+			// 100k lists
+			List<int> Sorted_100_000 = new List<int>();
+			List<int> Random_100_000 = new List<int>();
+			List<int> Reverse_100_000 = new List<int>();
+
+			// Sorted lists
+			for (int i = 1; i <= 1_000; i++) { Sorted_1000.Add(i); }
+			for (int i = 1; i <= 100_000; i++) { Sorted_100_000.Add(i); }
+
+			// Random lists
+			Random_1000 = Sorted_1000.ToList();
+			for (int i = Random_1000.Count - 1; i > 0; i--) {
+				int j = rand.Next(i + 1);
+				int temp = Random_1000[i];
+				Random_1000[i] = Random_1000[j];
+				Random_1000[j] = temp;
+			}
+			Random_100_000 = Sorted_100_000.ToList();
+		}
+	}
+}
