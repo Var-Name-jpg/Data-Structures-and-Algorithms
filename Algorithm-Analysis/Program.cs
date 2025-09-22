e87280906d03383487fcf9e94d86e1460e5b5375
Var-Name-jpg <ethanwalton0418@outlook.com>
2025-09-22 13:49:43 -0400

    fixed list caching for accurate benchamrking

--- Algorithm-Analysis/Program.cs
+++ Algorithm-Analysis/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime;
 
 namespace SortingBenchmark {
 	public class Program {
@@ -19,44 +20,69 @@ namespace SortingBenchmark {
 			// Diagnostic for generating numbers (for control)
 			Stopwatch generationStopwatch = Stopwatch.StartNew();
 
-			// 1k lists
+			/// 1k lists ///
+			
+			// Sorted
 			List<int> Sorted_1000 = new List<int>();
+			using (StreamReader sr = new StreamReader("Data/sorted_1000.txt")) {
+				string line;
+				while ((line = sr.ReadLine()) != null) {
+					Sorted_1000.Add(int.Parse(line));
+				}
+			}
+
+			// Random
 			List<int> Random_1000 = new List<int>();
-
-			// 100k lists
-			List<int> Sorted_100_000 = new List<int>();
-			List<int> Random_100_000 = new List<int>();
-
-			// Sorted lists
-			for (int i = 1; i <= 1_000; i++) { Sorted_1000.Add(i); }
-			for (int i = 1; i <= 100_000; i++) { Sorted_100_000.Add(i); }
-
-			// Random lists
-			Random_1000 = Sorted_1000.ToList();
-			for (int i = Random_1000.Count - 1; i > 0; i--) {
-				int j = rand.Next(i + 1);
-				int temp = Random_1000[i];
-				Random_1000[i] = Random_1000[j];
-				Random_1000[j] = temp;
-			}
-			Random_100_000 = Sorted_100_000.ToList();
-			for (int i = Random_100_000.Count - 1; i > 0; i--) {
-				int j = rand.Next(i + 1);
-				int temp = Random_100_000[i];
-				Random_100_000[i] = Random_100_000[j];
-				Random_100_000[j] = temp;
+			using (StreamReader sr = new StreamReader("Data/random_1000.txt")) {
+				string line;
+				while ((line = sr.ReadLine()) != null) {
+					Random_1000.Add(int.Parse(line));
+				}
 			}
 
-			// Reverse lists
+			// Reverse
 			List<int> Reverse_1000 = new List<int>();
+			using (StreamReader sr = new StreamReader("Data/reverse_1000.txt")) {
+				string line;
+				while ((line = sr.ReadLine()) != null) {
+					Reverse_1000.Add(int.Parse(line));
+				}
+			}
+
+			/// 100k lists ///
+
+			// Sorted
+			List<int> Sorted_100_000 = new List<int>();
+			using (StreamReader sr = new StreamReader("Data/sorted_100_000.txt")) {
+				string line;
+				while ((line = sr.ReadLine()) != null) {
+					Sorted_100_000.Add(int.Parse(line));
+				}
+			}
+
+			// Random
+			List<int> Random_100_000 = new List<int>();
+			using (StreamReader sr = new StreamReader("Data/random_100_000.txt")) {
+				string line;
+				while ((line = sr.ReadLine()) != null) {
+					Random_100_000.Add(int.Parse(line));
+				}
+			}
+
+			// Reverse
 			List<int> Reverse_100_000 = new List<int>();
+			using (StreamReader sr = new StreamReader("Data/reverse_100_000.txt")) {
+				string line;
+				while ((line = sr.ReadLine()) != null) {
+					Reverse_100_000.Add(int.Parse(line));
+				}
+			}
 
-			for (int i = 6_003; i > 1_000; i-=3) { Reverse_1000.Add(i); }
-			for (int i = 600_003; i > 100_000; i-=3) { Reverse_100_000.Add(i); }
-
+			
+			
 			// Parse words into a list
 			List<string> words = new List<string>();
-			using (StreamReader sr = new StreamReader("Words.txt")) {
+			using (StreamReader sr = new StreamReader("Data/Words.txt")) {
 				string line;
 
 				while ((line = sr.ReadLine()) != null) {
@@ -77,41 +103,12 @@ namespace SortingBenchmark {
 			Console.WriteLine("\nInsertion Sort w/ 1000 integers");
 			Console.WriteLine(new string('=', 20));
 
-			// Insertion sort with sorted list
-			List<int> insertionSorted_1000 = new List<int>(Sorted_1000);
-			
-			Stopwatch timer = Stopwatch.StartNew();
-			insertionSorted_1000 = Sorting_Algs.InsertionSort(insertionSorted_1000);
-			timer.Stop();
+			// Run benchmarks
+			double timeSorted_1000 = Benchmark(Sorted_1000, Sorting_Algs.InsertionSort);
+			double timeRandom_1000 = Benchmark(Random_1000, Sorting_Algs.InsertionSort);
+			double timeReverse_1000 = Benchmark(Reverse_1000, Sorting_Algs.InsertionSort);
+			double timeWords = Benchmark(words, Sorting_Algs.InsertionSort);
 
-			double timeSorted_1000 = timer.Elapsed.TotalMilliseconds;
-
-			// Insertion sort with random list
-			List<int> insertionRandom_1000 = new List<int>(Random_1000);
-			
-			timer = Stopwatch.StartNew();
-			insertionRandom_1000 = Sorting_Algs.InsertionSort(insertionRandom_1000);
-			timer.Stop();
-
-			double timeRandom_1000 = timer.Elapsed.TotalMilliseconds;
-
-			// Insetion sort with reverse list
-			List<int> insertionReverse_1000 = new List<int>(Reverse_1000);
-			
-			timer = Stopwatch.StartNew();
-			insertionReverse_1000 = Sorting_Algs.InsertionSort(insertionReverse_1000);
-			timer.Stop();
-
-			double timeReverse_1000 = timer.Elapsed.TotalMilliseconds;
-
-			// Insertion sort with text
-			List<string> insertionWords = new List<string>(words);
-			
-			timer = Stopwatch.StartNew();
-			insertionWords = Sorting_Algs.InsertionSort(insertionWords);
-			timer.Stop();
-
-			double timeWords = timer.Elapsed.TotalMilliseconds;
 
 			// Print the times for log
 			Console.WriteLine($"Sorted: {timeSorted_1000} ms");
@@ -127,41 +124,11 @@ namespace SortingBenchmark {
 			Console.WriteLine("\nQuick Sort w/ 1000 integers");
 			Console.WriteLine(new string('=', 20));
 
-			// Quick sort with sorted list
-			List<int> quickSorted_1000 = new List<int>(Sorted_1000);
-
-			timer = Stopwatch.StartNew();
-			quickSorted_1000 = Sorting_Algs.QuickSort(quickSorted_1000);
-			timer.Stop();
-
-			timeSorted_1000 = timer.Elapsed.TotalMilliseconds;
-
-			// Quick sort with random list
-			List<int> quickRandom_1000 = new List<int>(Random_1000);
-			
-			timer = Stopwatch.StartNew();
-			quickRandom_1000 = Sorting_Algs.QuickSort(quickRandom_1000);
-			timer.Stop();
-
-			timeRandom_1000 = timer.Elapsed.TotalMilliseconds;
-
-			// Quick sort with reverse list
-			List<int> quickReverse_1000 = new List<int>(Reverse_1000);
-			
-			timer = Stopwatch.StartNew();
-			quickReverse_1000 = Sorting_Algs.QuickSort(quickReverse_1000);
-			timer.Stop();
-
-			timeReverse_1000 = timer.Elapsed.TotalMilliseconds;
-
-			// Quick sort with text
-			List<string> quickWords = new List<string>(words);
-
-			timer = Stopwatch.StartNew();
-			quickWords = Sorting_Algs.QuickSort(quickWords);
-			timer.Stop();
-
-			timeWords = timer.Elapsed.TotalMilliseconds;
+			// Run benchmarks
+			timeSorted_1000 = Benchmark(Sorted_1000, Sorting_Algs.QuickSort);
+			timeRandom_1000 = Benchmark(Random_1000, Sorting_Algs.QuickSort);
+			timeReverse_1000 = Benchmark(Reverse_1000, Sorting_Algs.QuickSort);
+			timeWords = Benchmark(words, Sorting_Algs.QuickSort);
 
 			// Print the times for log
 			Console.WriteLine($"Sorted: {timeSorted_1000} ms");
@@ -170,5 +137,32 @@ namespace SortingBenchmark {
 			Console.WriteLine($"Words: {timeWords} ms");
 			Console.WriteLine(new string('=', 20));
 		}
+
+		static double Benchmark<T>(List<T> input, SortAlgorithm<T> sorter) where T : IComparable<T> {
+			// Force GC cleanup
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+
+			// Clone input to avoid mutation
+			var testInput = new List<T>(input);
+
+			// Warm-Up for JIT
+			sorter(new List<T>(testInput));
+
+			// Reset GC to isolate timing
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+
+			// Time benchmark
+			Stopwatch timer = Stopwatch.StartNew();
+			sorter(testInput);
+			timer.Stop();
+
+			return timer.Elapsed.TotalMilliseconds;
+		}
+
+		public delegate List<T> SortAlgorithm<T>(List<T> items) where T : IComparable<T>;
 	}
 }
